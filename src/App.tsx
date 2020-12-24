f3d9af2e7e7fcdc382eda752b07f08b7c74510e0
Yusuf Alakram <yak1998@gmail.com>
2020-12-24 23:12:31 +0300

    add population display

--- src/App.tsx
+++ src/App.tsx
@@ -2,51 +2,82 @@
 import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import Button from '@material-ui/core/Button'
 
-const hitsDeserializer = (data) => {
-  if (data === undefined) return []
-  return data.hits
+const fetchSuggestions = (query, setHits) => {
+  return fetch('https://places-dsn.algolia.net/1/places/query', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json',
+    },
+    body: JSON.stringify({
+      query: query,
+      type: 'country',
+      language: 'en',
+    }),
+  })
+    .then((response) => response.json())
+    .then((json) => (json === undefined ? [] : json.hits))
+    .then((hits) => setHits(hits))
 }
 
-const defaultCountryDeserializer = (hits): string[] => {
-  if (hits.length === 0) return []
-  console.log(hits)
-  return hits
-    .map((hit) => hit.locale_names[0])
-    .filter((value, index, self) => self.indexOf(value) === index)
+const formatNumber = (x: string): string => {
+  if (x === undefined) return ''
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
 export default function App() {
   const [FieldInput, setFieldInput] = useState('')
-  const [Suggestions, setSuggestions] = useState([])
+  const [Hits, setHits] = useState()
+  const [Country, setCountry] = useState()
+  const [Population, setPopulation] = useState()
 
   const fieldInputChanged = (event, values) => {
     setFieldInput(values)
-    fetch('https://places-dsn.algolia.net/1/places/query', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        accept: 'application/json',
-      },
-      body: JSON.stringify({
-        query: values,
-        type: 'country',
-        language: 'en',
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => hitsDeserializer(data))
-      .then((hits) => defaultCountryDeserializer(hits))
-      .then((countries) => setSuggestions(countries))
+    fetchSuggestions(values, setHits)
   }
 
   return (
-    <div>
+    <React.Fragment>
+      <h1>{Country}</h1>
+      <h2>
+        {Population === undefined
+          ? ''
+          : 'Population: ' + formatNumber(Population)}
+      </h2>
+      {Country !== '' && Country !== undefined ? (
+        <Button
+          variant="contained"
+          onClick={() => {
+            setCountry(undefined)
+            setPopulation(undefined)
+            setFieldInput('')
+          }}
+        >
+          Clear
+        </Button>
+      ) : (
+        ''
+      )}
       <Autocomplete
         freeSolo
-        options={Suggestions}
+        options={
+          Hits === undefined || Hits.length === 0
+            ? []
+            : Hits.map((hit) => hit.locale_names[0]).filter(
+                (value, index, self) => self.indexOf(value) === index,
+              )
+        }
         inputValue={FieldInput}
         onInputChange={fieldInputChanged}
+        onChange={(event, values) => {
+          const hit = Hits.filter((hit) => hit.locale_names[0] === values)[0]
+          if (hit !== undefined) {
+            setCountry(hit.locale_names[0])
+            setPopulation(hit.population)
+          }
+        }}
         renderInput={(params) => (
           <TextField
             {...params}
@@ -56,6 +87,6 @@ export default function App() {
           />
         )}
       />
-    </div>
+    </React.Fragment>
   )
 }
